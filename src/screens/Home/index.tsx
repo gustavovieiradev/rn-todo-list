2bae241f360de3da756d86752c206744786eebb9
Gustavo Vieira <gustavovieiradev@gmail.com>
2022-10-16 14:26:52 -0300

    feat: add logo and action component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, StatusBar, View } from 'react-native';
+import { Task } from '../../components/Task/input';
 import { styles } from './styles';
+
+import Logo from '../../assets/logo.png';
+import { Input } from '../../components/Input';
+import { CreateButton } from '../../components/Buttons/CreateButton';
 
 export const Home: React.FC = () => {
   return (
     <View style={styles.container}>
+      <StatusBar 
+        backgroundColor="transparent" 
+        barStyle="light-content" 
+        translucent 
+      />
       <View style={styles.header}>
-
+        <Image source={Logo} />
+      </View>
+      <View style={styles.content}>
+        <View style={styles.form}>
+          <Input />
+          <CreateButton />
+        </View>
       </View>
     </View>
   )
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,11 +1,21 @@
 import { StyleSheet } from "react-native";
+import theme from "../../../layout/theme";
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: theme.colors.gray600
   },
   header: {
     height: 173,
     backgroundColor: '#0D0D0D',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  content: {
+
+  },
+  form: {
+    
   }
 })
