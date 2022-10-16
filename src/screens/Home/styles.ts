c109e149e0649bc72ef59d37879be2f2cac75b46
Gustavo Vieira <gustavovieiradev@gmail.com>
2022-10-16 05:58:36 -0300

    first commit

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    height: 173,
+    backgroundColor: '#0D0D0D',
+  }
+})
